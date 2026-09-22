21a46d77b10b18c64c8d285f558716d88c2595cb
Sh3hu <ehinderomartins@gmail.com>
2026-09-22 20:48:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='shehu-fitness-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
